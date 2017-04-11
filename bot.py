7bfdada0cbb6b3951ec4acc7f4734f02a69f3a85
ms506654 <stevemalalel@gmail.com>
2017-04-11 18:55:15 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import sys, socket, os, select, time
+import sys, socket, os, select, time , re
 
 #Programme qui permet de tester le bon fonctionnement de chat client/serveur
 #Test :
@@ -98,11 +98,78 @@ def test2():
     client1.close()
     client2.close()
 
+def test3() :
+    client1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client1.connect((host, port))
+    client1.send("bot1")
+    client2.connect((host, port))
+    client2.send("bot2")
+    client3.connect((host, port))
+    client3.send("bot3")
+    time.sleep(1)
+    message_de_connexion_1 = client1.recv(4096)
+    message_de_connexion_2 = client2.recv(4096)
+    message_de_connexion_3 = client3.recv(4096)
+    message_test_1 = "Coucou\n"
+    message_test_2 = "Salut\n"
+    try :
+        client1.send(message_test_1)
+        client2.send(message_test_2)
+        print("Test envoi multiple : ok")
+    except :
+        print("Test envoi multiple : fail")
+        sys.exit()
+    test = True
+    while test :
+        socketlist = [client1, client2, client3]
+        ready, write, error = select.select(socketlist, [], [])
+        for s in ready :
+            if s == client3 :
+                message_1 = s.recv(4096)
+                message_2 = s.recv(4096)
+                if ((message_1 == "\rbot1 >> " + message_test_1 or message_1 == "\rbot2 >> " + message_test_2) and (message_2 == "\rbot1 >> " + message_test_1 or message_2 == "\rbot2 >> " + message_test_2)) :
+                    print("Test réception multiple : ok")
+                    test = False
+                else :
+                    print("Test réception multiple : fail")
+                    sys.exit()
+    client1.send("\quit")
+    time.sleep(1)
+    client2.send("\quit")
+    time.sleep(1)
+    client3.send("\quit")
+    time.sleep(1)
+    client1.close()
+    client2.close()
+    client3.close()
+
+#test4() pour connexion web
+
+def test5() :
+    client1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client1.connect((host, port))
+    client1.send("bot1")
+    time.sleep(1)
+    message_de_connexion_1 = client1.recv(4096)
+    client1.send("\info\n")
+    message_1 = client1.recv(4096)
+    print(message_1) #pour voir visuellement le message reçu , à enlever
+    if re.match(r"(.)*Information(.)*",message_1) is not None : #http://apprendre-python.com/page-expressions-regulieres-regular-python
+        print("Test commande \\info : ok")
+    else :
+        print("Test commande \\info : fail")
+    
+
 pid = os.fork()
 if pid == 0:
     os.system("xterm -e python server.py 2555 2556")
 else:
     time.sleep(2)
-    test1()
+    test1() #à revoir
     test2()
-    sys.exit()
+    test3()
+    #test4()
+    test5() #à revoir
+sys.exit()
